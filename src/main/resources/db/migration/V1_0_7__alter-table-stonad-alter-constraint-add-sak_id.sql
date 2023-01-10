d37df573d26ae5209770de52bd84c263a24e3e99
rinnan17 <magnus.rinnan@nav.no>
2023-01-10 13:36:41 +0100

    Retter script

--- src/main/resources/db/migration/V1_0_7__alter-table-stonad-alter-constraint-add-sak_id.sql
+++ src/main/resources/db/migration/V1_0_7__alter-table-stonad-alter-constraint-add-sak_id.sql
@@ -2,4 +2,4 @@ ALTER TABLE stonad
     DROP CONSTRAINT stonad_stonad_type_skyldner_id_kravhaver_id_key ;
 
 ALTER TABLE stonad
-    ADD UNIQUE (stonad_type, skyldner_id, kravhaver_id, sak_id) ;
+    ADD UNIQUE (type, skyldner_id, kravhaver_id, sak_id) ;
