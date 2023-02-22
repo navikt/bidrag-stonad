dbd32e50b02856fd50f7fd9024947f8c238bdca7
rinnan17 <magnus.rinnan@nav.no>
2023-02-22 11:58:00 +0100

    Lagt til sjekk på om stønad eksistrerer før oppdatering av RM, logger hvis ikke. Også lagt til @Transactional på BehandleHendelseService

--- src/main/kotlin/no/nav/bidrag/stonad/service/BehandleHendelseService.kt
+++ src/main/kotlin/no/nav/bidrag/stonad/service/BehandleHendelseService.kt
@@ -12,6 +12,7 @@ import no.nav.bidrag.behandling.felles.enums.VedtakType
 import no.nav.bidrag.stonad.SECURE_LOGGER
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -22,13 +23,13 @@ interface BehandleHendelseService {
 }
 
 @Service
+@Transactional
 class DefaultBehandleHendelseService(
-  private val stonadService: StonadService,
-  private val persistenceService: PersistenceService
+    private val stonadService: StonadService,
+    private val persistenceService: PersistenceService
 ) : BehandleHendelseService {
 
   override fun behandleHendelse(vedtakHendelse: VedtakHendelse) {
-    LOGGER.info("Behandler vedtakHendelse for vedtakid: ${vedtakHendelse.id}")
     SECURE_LOGGER.info("Behandler vedtakHendelse: $vedtakHendelse")
 
     vedtakHendelse.stonadsendringListe?.forEach() { stonadsendring ->
@@ -41,7 +42,7 @@ class DefaultBehandleHendelseService(
 //    Sjekker om stønad skal oppdateres
     if (stonadsendring.endring && stonadsendring.innkreving == Innkreving.JA) {
       val eksisterendeStonad = stonadService.hentStonad(
-        HentStonadRequest(stonadsendring.type, stonadsendring.sakId, stonadsendring.skyldnerId, stonadsendring.kravhaverId)
+          HentStonadRequest(stonadsendring.type, stonadsendring.sakId, stonadsendring.skyldnerId, stonadsendring.kravhaverId)
       )
 
       if (eksisterendeStonad != null) {
@@ -53,10 +54,14 @@ class DefaultBehandleHendelseService(
           endreStonad(eksisterendeStonad, stonadsendring, vedtakId, opprettetAv, vedtakTidspunkt)
         }
       } else {
-        opprettStonad(stonadsendring, vedtakId, opprettetAv, vedtakTidspunkt)
+        //Stønaden finnes ikke fra , hvis det er forsøkt endret mottaker for stønad som ikke finnes så skal det logges, men ikke feile.
+        if (vedtakType == VedtakType.ENDRING_MOTTAKER) {
+          SECURE_LOGGER.info("Mottaker forsøkt endret for stønad som ikke finnes $vedtakId")
+        } else {
+          opprettStonad(stonadsendring, vedtakId, opprettetAv, vedtakTidspunkt)
+        }
       }
     } else {
-      LOGGER.info("Stønad ikke oppdatert pga innkreving = nei eller endring = false: $vedtakId")
       SECURE_LOGGER.info("Stønad ikke oppdatert pga innkreving = nei eller endring = false: $vedtakId")
     }
   }
@@ -69,33 +74,33 @@ class DefaultBehandleHendelseService(
     var i = 1
     hendelsePeriodeListe.forEach {
       periodeListe.add(
-        OpprettStonadPeriodeRequestDto(
-          periodeFom = it.fomDato,
-          periodeTil = finnPeriodeTil(it.tilDato, hendelsePeriodeListe, i),
-          vedtakId = vedtakId,
-          gyldigFra = vedtakTidspunkt,
-          gyldigTil = null,
-          periodeGjortUgyldigAvVedtakId = null,
-          belop = it.belop,
-          valutakode = it.valutakode,
-          resultatkode = it.resultatkode
-        )
+          OpprettStonadPeriodeRequestDto(
+              periodeFom = it.fomDato,
+              periodeTil = finnPeriodeTil(it.tilDato, hendelsePeriodeListe, i),
+              vedtakId = vedtakId,
+              gyldigFra = vedtakTidspunkt,
+              gyldigTil = null,
+              periodeGjortUgyldigAvVedtakId = null,
+              belop = it.belop,
+              valutakode = it.valutakode,
+              resultatkode = it.resultatkode
+          )
       )
       i++
     }
 
     val oppdatertStonad =
-      OpprettStonadRequestDto(
-        type = stonadsendring.type,
-        sakId = stonadsendring.sakId,
-        skyldnerId = stonadsendring.skyldnerId,
-        kravhaverId = stonadsendring.kravhaverId,
-        mottakerId = stonadsendring.mottakerId,
-        indeksreguleringAar = stonadsendring.indeksreguleringAar,
-        innkreving = stonadsendring.innkreving,
-        opprettetAv = opprettetAv,
-        periodeListe = periodeListe
-      )
+        OpprettStonadRequestDto(
+            type = stonadsendring.type,
+            sakId = stonadsendring.sakId,
+            skyldnerId = stonadsendring.skyldnerId,
+            kravhaverId = stonadsendring.kravhaverId,
+            mottakerId = stonadsendring.mottakerId,
+            indeksreguleringAar = stonadsendring.indeksreguleringAar,
+            innkreving = stonadsendring.innkreving,
+            opprettetAv = opprettetAv,
+            periodeListe = periodeListe
+        )
 
     stonadService.endreStonad(eksisterendeStonad, oppdatertStonad, vedtakTidspunkt)
 
@@ -107,33 +112,33 @@ class DefaultBehandleHendelseService(
     var i = 1
     hendelsePeriodeListe.forEach {
       periodeListe.add(
-        OpprettStonadPeriodeRequestDto(
-          periodeFom = it.fomDato,
-          periodeTil = finnPeriodeTil(it.tilDato, hendelsePeriodeListe, i),
-          vedtakId = vedtakId,
-          gyldigFra = vedtakTidspunkt,
-          gyldigTil = null,
-          periodeGjortUgyldigAvVedtakId = null,
-          belop = it.belop,
-          valutakode = it.valutakode,
-          resultatkode = it.resultatkode
-        )
+          OpprettStonadPeriodeRequestDto(
+              periodeFom = it.fomDato,
+              periodeTil = finnPeriodeTil(it.tilDato, hendelsePeriodeListe, i),
+              vedtakId = vedtakId,
+              gyldigFra = vedtakTidspunkt,
+              gyldigTil = null,
+              periodeGjortUgyldigAvVedtakId = null,
+              belop = it.belop,
+              valutakode = it.valutakode,
+              resultatkode = it.resultatkode
+          )
       )
       i++
     }
 
     stonadService.opprettStonad(
-      OpprettStonadRequestDto(
-        type = stonadsendring.type,
-        sakId = stonadsendring.sakId,
-        skyldnerId = stonadsendring.skyldnerId,
-        kravhaverId = stonadsendring.kravhaverId,
-        mottakerId = stonadsendring.mottakerId,
-        indeksreguleringAar = stonadsendring.indeksreguleringAar,
-        innkreving = stonadsendring.innkreving,
-        opprettetAv = opprettetAv,
-        periodeListe = periodeListe
-      )
+        OpprettStonadRequestDto(
+            type = stonadsendring.type,
+            sakId = stonadsendring.sakId,
+            skyldnerId = stonadsendring.skyldnerId,
+            kravhaverId = stonadsendring.kravhaverId,
+            mottakerId = stonadsendring.mottakerId,
+            indeksreguleringAar = stonadsendring.indeksreguleringAar,
+            innkreving = stonadsendring.innkreving,
+            opprettetAv = opprettetAv,
+            periodeListe = periodeListe
+        )
     )
   }
 
