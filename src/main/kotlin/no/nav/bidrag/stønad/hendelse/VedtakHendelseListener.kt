c5cbccb02ec78f383a0776b18cfd698c2e92728f
Ugur Alpay Cenar <cenar93@gmail.com>
2024-09-19 12:55:17 +0200

    Opprett bidrag-stonad syntetisk

--- src/main/kotlin/no/nav/bidrag/stønad/hendelse/VedtakHendelseListener.kt
+++ src/main/kotlin/no/nav/bidrag/stønad/hendelse/VedtakHendelseListener.kt
@@ -34,7 +34,7 @@ open class PojoVedtakHendelseListener(
 
 open class KafkaVedtakHendelseListener(jsonMapperService: JsonMapperService, behandeHendelseService: BehandleHendelseService) :
     PojoVedtakHendelseListener(jsonMapperService, behandeHendelseService) {
-    @KafkaListener(groupId = "\${NAIS_APP_NAME}", topics = ["\${TOPIC_VEDTAK}"], errorHandler = "vedtakshendelseErrorHandler")
+    @KafkaListener(groupId = "bidrag-stonad", topics = ["\${TOPIC_VEDTAK}"], errorHandler = "vedtakshendelseErrorHandler")
     override fun lesHendelse(hendelse: String) {
         super.lesHendelse(hendelse)
     }
